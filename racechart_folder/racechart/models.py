fb5e9a1b388e21f6138b62f29b9678a4b1bcf1b9
Trizzlenova <tdswayzee@gmail.com>
2018-06-13 15:36:36 -0700

    got first model working

--- racechart_folder/racechart/models.py
+++ racechart_folder/racechart/models.py
@@ -1,18 +1,70 @@
 from django.db import models
-# from .api_calls import drivers
+from json import *
 
 class Driver(models.Model):
-  name = models.CharField(max_length=100)
-
+  full_name = models.CharField(max_length=100)
+  birth_place = models.CharField(max_length=500)
+  birthday = models.DateField()
+  country = models.CharField(max_length=100)
+  # cars
   def __str__(self):
-    return self.name
+    return self.full_name
 
   @classmethod
-  def create(cls, name):
-      driver = cls(name=name)
+  def create(cls, full_name, birth_place, birthday, country):
+      driver = cls(full_name=full_name, birth_place=birth_place, birthday=birthday, country=country)
       # do something with the driver
       return driver
 
-sue = Driver.create('Sue Redneck')
-sue.save()
-print(f'I am printing {sue.name}')
+
+# tommy = Driver.create('Tommy Trucker', 'Mobile, Alabama', '1985-01-01', 'Murica')
+# tommy.save()
+# print(f'I am printing {tommy.full_name} from {tommy.country}')
+# "UNITED STATES"
+# first_name
+# :
+# "David"
+# full_name
+# :
+# "David Ragan"
+# gender
+# :
+# "M"
+# height
+# :
+# 72
+# hobbies
+# :
+# "Boxing, restoring old cars, hunting, fishing"
+# id
+# :
+# "25884070-3ded-4fdc-9370-d09428ac95e4"
+# last_name
+# :
+# "Ragan"
+# points_eligible
+# :
+# true
+# residence
+# :
+# "Huntersville, North Carolina, United States"
+# rookie_year
+# :
+# 2007
+# status
+# :
+# "ACT"
+# team
+# :
+# {id: "bd38dfec-bb97-4f29-b032-cc6f0c77dfe6", name: "Front Row Motorsports"}
+# twitter
+# :
+# "@DavidRagan"
+
+#
+
+# # for loops will be dope
+
+# # sue = Driver.create('Sue Redneck')
+# # sue.save()
+# # print(f'I am printing {sue.name}')

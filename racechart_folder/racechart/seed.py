afaa67b6183691e4974760191892a78ecf97c761
maxvidrine <max.vidrine@gmail.com>
2018-06-17 22:32:29 -0700

    updated worker with two additional tasks

--- racechart_folder/racechart/seed.py
+++ racechart_folder/racechart/seed.py
@@ -3,10 +3,10 @@ from racechart.models import Driver, Result, Race, Team, Standing
 import json
 
 
-                        # ########################## #
-                        #       LOAD JSON FILES      #
-                        # ########################## #
-      
+						# ########################## #
+						#       LOAD JSON FILES      #
+						# ########################## #
+
 race_json = open('racechart/json/race.json').read()
 loaded_race = json.loads(race_json)
 
@@ -16,9 +16,9 @@ loaded_drivers = json.loads(driver_json)
 standings_json = open('racechart/json/standings.json').read()
 loaded_standings = json.loads(driver_json)
 
-                        # ########################## #
-                        #     CLEAR THE DATABASE     #
-                        # ########################## #
+						# ########################## #
+						#     CLEAR THE DATABASE     #
+						# ########################## #
 
 Driver.objects.all().delete()
 Team.objects.all().delete()
@@ -31,9 +31,9 @@ print('cleared the database')
 drivers_array = loaded_drivers['drivers']
 necessary_race_keys = []
 necessary_standing_keys = []
-                        # ########################## #
-                        #       CLEAN THE DATA       #
-                        # ########################## #
+						# ########################## #
+						#       CLEAN THE DATA       #
+						# ########################## #
 
 
 cleaned_teams = []
@@ -46,21 +46,21 @@ team_array = []
 for individual_driver in drivers_array:
 
   team = {
-    # 'crew_chief': None,
-    'manufacturer': None,
-    'owner': None,
-    'name': individual_driver['team']['name'],
-    'sponsors': None,
+	# 'crew_chief': None,
+	'manufacturer': None,
+	'owner': None,
+	'name': individual_driver['team']['name'],
+	'sponsors': None,
   }
 
   for key in individual_driver:
 
-    # find the array of cars, and
-    if type(individual_driver[key]) == list and len(individual_driver[key]) > 0:
-      # team['crew_chief'] = individual_driver['cars'][0]['crew_chief']
-      team['manufacturer'] = individual_driver['cars'][0]['manufacturer']['name']
-      team['owner'] = individual_driver['cars'][0]['owner']['name']
-      team['sponsors'] = individual_driver['cars'][0]['sponsors']
+	# find the array of cars, and
+	if type(individual_driver[key]) == list and len(individual_driver[key]) > 0:
+	  # team['crew_chief'] = individual_driver['cars'][0]['crew_chief']
+	  team['manufacturer'] = individual_driver['cars'][0]['manufacturer']['name']
+	  team['owner'] = individual_driver['cars'][0]['owner']['name']
+	  team['sponsors'] = individual_driver['cars'][0]['sponsors']
 
   team_array.append(team)
 # print(len(team_array))
@@ -72,12 +72,12 @@ for added_team in team_array:
 
   for cleaned_team in cleaned_teams:
 
-    if added_team['name'] == cleaned_team['name']:
-      match_found = True
+	if added_team['name'] == cleaned_team['name']:
+	  match_found = True
 
   if match_found == False:
-      # print(team)
-    cleaned_teams.append(added_team)
+	  # print(team)
+	cleaned_teams.append(added_team)
 
 
 for team in cleaned_teams:
@@ -114,14 +114,14 @@ for single_driver in drivers_array:
   # Check if the drivers contain the keys needed
   for wanted_key in necessary_driver_keys:
 
-    match_found = False
+	match_found = False
 
-    for key in provided_keys:
-      if wanted_key == key:
-        match_found = True
-      # if no match is found, add key name to array
-    if match_found == False:
-      keys_to_add.append(wanted_key)
+	for key in provided_keys:
+	  if wanted_key == key:
+		match_found = True
+	  # if no match is found, add key name to array
+	if match_found == False:
+	  keys_to_add.append(wanted_key)
 
 
 # Add missing keys to driver.
@@ -129,15 +129,15 @@ for single_driver in drivers_array:
 
   for key in single_driver:
 
-    # find the array of cars, and
-    if type(single_driver[key]) == list and len(single_driver[key]) > 0:
+	# find the array of cars, and
+	if type(single_driver[key]) == list and len(single_driver[key]) > 0:
 
-      car_number = single_driver[key][0]['number']
-      single_driver['car_number'] = car_number
+	  car_number = single_driver[key][0]['number']
+	  single_driver['car_number'] = car_number
 
   if len(keys_to_add) > 0:
-    for key in keys_to_add:
-      single_driver[key] = None
+	for key in keys_to_add:
+	  single_driver[key] = None
 
   cleaned_drivers.append(single_driver)
 
@@ -147,10 +147,10 @@ for cleaned_driver in cleaned_drivers:
   new_driver.save()
   # print(new_driver)
 
-                ######################
-                ######## RACES #######
-                ######################
-        
+				######################
+				######## RACES #######
+				######################
+
 results = loaded_race['results']
 race_drivers = []
 

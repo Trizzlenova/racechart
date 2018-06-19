6da0ea4d8cbb9219c1eedf04dffbcff07dc2050e
Trizzlenova <tdswayzee@gmail.com>
2018-06-18 21:58:57 -0700

    added more html

--- racechart_folder/racechart/seed.py
+++ racechart_folder/racechart/seed.py
@@ -2,20 +2,6 @@ from json import *
 from racechart.models import Driver, Result, Race, Team, Standing
 import json
 
-
-                        # ########################## #
-                        #       LOAD JSON FILES      #
-                        # ########################## #
-
-race_json = open('racechart/json/race.json').read()
-loaded_race = json.loads(race_json)
-
-driver_json = open('racechart/json/drivers.json').read()
-loaded_drivers = json.loads(driver_json)
-
-standings_json = open('racechart/json/standings.json').read()
-loaded_standings = json.loads(driver_json)
-
                         # ########################## #
                         #     CLEAR THE DATABASE     #
                         # ########################## #
@@ -63,23 +49,6 @@ def seed_teams():
 
 # Check to make sure we don't make identical teams
 
-for added_team in team_array:
-  match_found = False
-
-  for cleaned_team in cleaned_teams:
-
-    if added_team['name'] == cleaned_team['name']:
-      match_found = True
-
-  if match_found == False:
-      # print(team)
-    cleaned_teams.append(added_team)
-
-
-for team in cleaned_teams:
-  new_team = Team.create(team)
-  new_team.save()
-  # print(new_team)
     for added_team in team_array:
       match_found = False
 
@@ -95,62 +64,6 @@ for team in cleaned_teams:
       new_team = Team.create(team)
       new_team.save()
       print(new_team)
-
-# iterate through each driver
-for single_driver in drivers_array:
-
-  # declare values in case any drivers are missing
-  single_driver['car_number'] = None
-
-  # array of values required for model
-  necessary_driver_keys = ['full_name','birth_place', 'birthday', 'country', 'gender', 'height', 'hobbies', 'last_name', 'residence', 'rookie_year', 'status', 'twitter',]
-
-#### assign teams
-
-  team_name = single_driver['team']['name']
-  associated_team = Team.objects.get(name=team_name)
-
-  single_driver['team'] = associated_team
-
-  # keys provided by API
-  provided_keys = list(single_driver.keys())
-  keys_to_add = []
-
-  # Check if the drivers contain the keys needed
-  for wanted_key in necessary_driver_keys:
-
-    match_found = False
-
-    for key in provided_keys:
-      if wanted_key == key:
-        match_found = True
-      # if no match is found, add key name to array
-    if match_found == False:
-      keys_to_add.append(wanted_key)
-
-
-# Add missing keys to driver.
-# Also, make car_number match the number of first car in driver's array of cars
-
-  for key in single_driver:
-
-    # find the array of cars, and
-    if type(single_driver[key]) == list and len(single_driver[key]) > 0:
-
-      car_number = single_driver[key][0]['number']
-      single_driver['car_number'] = car_number
-
-  if len(keys_to_add) > 0:
-    for key in keys_to_add:
-      single_driver[key] = None
-
-  cleaned_drivers.append(single_driver)
-
-for cleaned_driver in cleaned_drivers:
-
-  new_driver = Driver.create(cleaned_driver)
-  new_driver.save()
-  print(new_driver)
 
                 ######################
                 ####### DRIVERS ######
@@ -199,16 +112,9 @@ def seed_drivers():
       new_driver.save()
       print(new_driver)
 
-
                 ######################
                 ######## RACES #######
                 ######################
-
-# results = loaded_race['results']
-# race_drivers = []
-
-# new_race = Race.create(loaded_race)
-# new_race.save()
 def seed_races():
     cleaned_races = []
     i = 0
@@ -302,12 +208,6 @@ def seed_standings():
             if type(standing[key]) == float:
                 standing[key] = round(standing[key], 2)
 
-
-# results = loaded_race['results']
-# race_drivers = []
-
-# new_race = Race.create(loaded_race)
-# new_race.save()
         new_standing = Standing.create(standing)
         new_standing.save()
         print(new_standing)

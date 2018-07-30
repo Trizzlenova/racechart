86a88d6beca596c29164fe8fd902600000150c03
bcorman <benjamincorman@gmail.com>
2018-07-30 14:53:55 -0700

    initial commit for push 2

--- racechart_folder/racechart_folder/settings.py
+++ racechart_folder/racechart_folder/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.environ['SECRET_KEY']
+SECRET_KEY=SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp']
+ALLOWED_HOSTS = []
 
 
 # Application definition

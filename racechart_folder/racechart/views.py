aecd367f263c530e3590c28918886db2d7ea406c
Trizzlenova <tdswayzee@gmail.com>
2018-08-23 14:36:39 -0700

    changed header image and put in nav

--- racechart_folder/racechart/views.py
+++ racechart_folder/racechart/views.py
@@ -137,11 +137,11 @@ def result_detail(request, pk):
     result = Result.objects.get(id=pk)
     return render(request, {'result': result})
 
-def error_404(request):
+def error_404(request, exception):
         data = {}
         return render(request,'racechart/error_404.html', data)
 
-def error_500(request):
+def error_500(request, exception):
         data = {}
         return render(request,'racechart/error_500.html', data)
 
